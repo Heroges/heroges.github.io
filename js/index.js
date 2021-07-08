b9584fc661ed90c343f707acb524308014bf5af0
Heroges <77005710+Heroges@users.noreply.github.com>
2021-07-08 23:22:01 +0700

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 class UiCheckbox extends HTMLElement {
-
   connectedCallback() {
     setTimeout(() => {
       this.innerHTML = ""
@@ -10,7 +9,10 @@ class UiCheckbox extends HTMLElement {
       labelName.insertAdjacentHTML("afterbegin", "<input type='checkbox'><span class='checkbox-span'><svg xmlns='http:\/\/www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'><path stroke-linecap=' stroke-linejoin=' stroke-width='2' d='M5 13l4 4L19 7' /></svg></span><span class='checkbox-text'>" + this.attributes.label.value + "</span>")
     })
   }
+}
 
+class UiInput extends HTMLElement {
+    
 }
 
 customElements.define('ui-checkbox', UiCheckbox);
