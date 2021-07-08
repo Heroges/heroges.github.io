62a5e12a1678c01ab58f22acb63a27080a2e0222
Heroges <77005710+Heroges@users.noreply.github.com>
2021-07-08 21:16:03 +0700

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,1 +0,0 @@
-const vuetify=new Vuetify,app=new Vue({el:"#root",vuetify:new Vuetify,data:{sidebar_toggle:!1,search:!1,search_text:"",title:"Главная",group:null,attrs:null,config:{sidebar:[{item:["mdi-home","Главная"]},{item:["mdi-view-list","Разделы"]},{item:["mdi-plus-circle-outline","Добавить"]}]}},methods:{setTitle:function(e){this.title=e}},beforeMount:function(){}});
